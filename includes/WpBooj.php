9b7ef9e8aff8f1dfb2728868879ceeff43abbed5
Alix Fullerton <alix@politeauthority.com>
2015-01-07 23:00:36 -0700

    Adding twitter status integration

--- includes/WpBooj.php
+++ includes/WpBooj.php
@@ -281,6 +281,13 @@ class WpBooj {
     return $results;
   }
 
+  public static function get_twitter_widget( $twitter_handle ){
+    $script = '<a class="twitter-timeline" href="https://twitter.com/'.$twitter_handle.'" data-widget-id="553063792964153344">Tweets by @'.$twitter_handle.'</a>
+      <script>
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+      </script>';
+    return $script;
+  }
 
   /************************************************************
      _____           _ 
